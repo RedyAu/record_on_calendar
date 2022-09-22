f43b67bca5b454c17d99b68d637f16e00ea89340
RedyAu <benedek@fodor.pro>
2022-09-22 21:24:15 +0200

    start recordable overhaul

--- bin/utils/recordable.dart
+++ bin/utils/recordable.dart
@@ -19,8 +19,7 @@ class Recordable {
   String title;
   String description;
 
-  Recordable(this.uid, this.start, this.end, this.title, this.description,
-      [this.recorderProcess]);
+  Recordable(this.uid, this.start, this.end, this.title, this.description);
 
   Process? recorderProcess;
   File? audioFile;
